d3e0726ad017bbae1866a1978c41b6deced2aa03
Sergey <thextreme.30@gmail.com>
2023-03-04 19:45:14 +0600

    created chat app

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # third party apps
+    'rest_framework',
+
+    # project apps
+    'chat.apps.ChatConfig',
 ]
 
 MIDDLEWARE = [
